5f7e6bddce7034a9f96fc93250493832b81a6e76
Naveen Kumar S <156082282+Naveenr810953@users.noreply.github.com>
2025-12-08 22:31:12 +0530

    script.js

--- Weather_App/script.js
+++ Weather_App/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () =>{
     const temperatureDisplay = document.getElementById("temperature");
     const descriptionDisplay = document.getElementById("description");
     const errorMessage = document.getElementById("error-message");
-
+     
     const API_KEY = "5f56d525d1619d0a2cd2eac4ce55588e";
 
     getweatherBtn.addEventListener("click", async () =>{
