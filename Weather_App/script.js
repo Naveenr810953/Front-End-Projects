56be5c7d1f4caa6e5cb5dc8d993defbe2490c1c3
Naveen Kumar S <156082282+Naveenr810953@users.noreply.github.com>
2026-01-01 22:15:40 +0530

    script.js

--- Weather_App/script.js
+++ Weather_App/script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () =>{
     const errorMessage = document.getElementById("error-message");
      
     const API_KEY = "5f56d525d1619d0a2cd2eac4ce55588e";
-
+   
     getweatherBtn.addEventListener("click", async () =>{
         const city = cityInput.value.trim();
 
